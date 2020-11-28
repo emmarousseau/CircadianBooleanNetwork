a2b5aa862fe760f72c0d712b473421d79da0e756
Pouki1999 <gogo.maslyankov@gmail.com>
2020-11-28 14:32:31 -0500

    Add files via upload

--- BooleanOpt.py
+++ BooleanOpt.py
@@ -7,14 +7,24 @@ Created on Sat Nov 28 14:07:16 2020
 
 class Edge:
     
-    def __init__(self, signal_delay):
+    def __init__(self, signal_delay, start, end, function):
         
         self.delay = signal_delay
+        self. start = start
+        self.end = end
+        self.function = function
         
         
 class Node:
     
-    def __init_(self, threshold):
+    def __init_(self, threshold, entering, exiting):
         
         self.threshold = threshold
+        self.entering_edges = []
+        self.exiting_edges = []
         
+        for edge in entering:
+            self.entering_edges.append(edge)
+            
+        for edge in exiting:
+            self.exiting_edges.append(edge)
