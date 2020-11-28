5851b2bd0d140df5381c67463452cfcd8caf36e1
Pouki1999 <gogo.maslyankov@gmail.com>
2020-11-28 14:23:53 -0500

    Add files via upload

---
+++ BooleanOpt.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 28 14:07:16 2020
+
+@author: gogom
+"""
+
+class Edge:
+    
+    def __init__(self, signal_delay):
+        
+        self.delay = signal_delay
+        
+        
+class Node:
+    
+    def __init_(self, threshold):
+        
+        self.threshold = threshold
+        
